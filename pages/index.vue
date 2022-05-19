4639235e9cb69c2440ec4e80f36042b5d68b8c80
Jared Dantis <jareddantis@gmail.com>
2022-05-19 12:27:36 +0800

    pages/index: Integrate navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial />
+  <div class="bg-white w-full">
+    <!-- Navbar -->
+    <NavigationBar is-logged-in />
+
+    <!-- Main content -->
+    <div class="max-w-6xl w-full mx-auto p-3">
+      <h1 class="text-6xl text-center font-bold">Home</h1>
+
+      <!-- Blog posts -->
+      <article class="prose">
+        <h1>Hello world</h1>
+        <p>Good morning</p>
+      </article>
+    </div>
+  </div>
 </template>
 
 <script>
+import NavigationBar from '~/components/NavigationBar.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { NavigationBar }
 }
 </script>
