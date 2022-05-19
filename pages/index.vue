615c7c714e46b948a307fae57e76fbb22c7cb65a
Jared Dantis <jareddantis@gmail.com>
2022-05-19 14:53:54 +0800

    index: Added smaller max-width and layout for post author

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,22 @@
     <NavigationBar is-logged-in />
 
     <!-- Main content -->
-    <div class="max-w-6xl w-full mx-auto p-3">
-      <h1 class="text-6xl text-center font-bold">Home</h1>
+    <div class="md:max-w-2xl max-w-xl w-full mx-auto py-3 px-6">
+      <h1 class="md:text-6xl text-4xl text-center font-bold">My blog</h1>
+      <hr class="my-8">
 
       <!-- Blog posts -->
-      <article class="prose">
-        <h1>Hello world</h1>
-        <p>Good morning</p>
+      <article class="prose mx-auto max-w-full w-full">
+        <h1 class="mb-0">Hello world</h1>
+        <h3 class="text-teal-500 mt-0 mb-4">by Author &#8212; May 21, 2022</h3>
+        <p class="font-serif">
+          Morbi placerat congue nisi et eleifend. Nam cursus cursus orci eget molestie.
+          Donec libero lorem, porta quis gravida id, tempor ut arcu. Mauris in posuere odio.
+          In in eleifend augue. Phasellus vitae egestas tellus. Mauris non posuere elit,
+          ut tincidunt felis. Donec sit amet leo vel ante congue vulputate et ut elit.
+          Nam ipsum nisi, ultrices vitae ullamcorper non, luctus id lectus. Vivamus vulputate
+          nec massa at laoreet. Etiam sit amet blandit purus.
+        </p>
       </article>
     </div>
   </div>
