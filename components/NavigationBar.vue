be4440aceba6a4c6c1dc8de2fca228793e99d654
Jared Dantis <jareddantis@gmail.com>
2022-05-19 12:27:21 +0800

    components: Begin work on navbar

---
+++ components/NavigationBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="bg-white w-full">
+    <div class="flex flex-row flex-wrap justify-between items-center max-w-6xl w-full mx-auto p-3">
+      <a href="#" class="flex items-center">
+        <img class="h-8 mr-4" src="/logo/1x/JotLogo.png" alt="Jot">
+        <span class="self-center text-xl font-semibold text-black">Jot</span>
+      </a>
+      <div>
+        <button v-if="isLoggedIn"
+          type="button"
+          class="text-teal-500 bg-white border-2 border-teal-500 hover:border-teal-800 focus:ring-4
+                focus:ring-teal-300 hover:text-teal-800 font-medium rounded-lg text-sm px-5 py-2.5
+                font-bold">
+          Log out
+        </button>
+        <button v-else
+          type="button"
+          class="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300
+                font-medium rounded-lg text-sm px-5 py-2.5">
+          Log in
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar',
+  props: {
+    isLoggedIn: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
