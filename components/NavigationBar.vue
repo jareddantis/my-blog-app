d845d5b19efe1705d3c8d1d53b397c920704531e
Jared Dantis <jareddantis@gmail.com>
2022-05-19 14:58:03 +0800

    NavigationBar: Show dashboard link for non-dashboard pages

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -6,17 +6,22 @@
         <span class="self-center text-xl font-semibold text-black">Jot</span>
       </a>
       <div>
+        <button v-if="isLoggedIn && !isDashboard"
+          type="button"
+          class="text-white bg-teal-600 hover:bg-teal-800 border-2 border-teal-600 hover:border-teal-800
+                focus:ring-4 focus:ring-teal-300 font-bold rounded-lg text-sm px-5 py-2.5 mr-3">
+          Dashboard
+        </button>
         <button v-if="isLoggedIn"
           type="button"
-          class="text-teal-500 bg-white border-2 border-teal-500 hover:border-teal-800 focus:ring-4
-                focus:ring-teal-300 hover:text-teal-800 font-medium rounded-lg text-sm px-5 py-2.5
-                font-bold">
+          class="text-teal-600 bg-white border-2 border-teal-600 hover:border-teal-800 focus:ring-4
+                focus:ring-teal-300 hover:text-teal-800 font-bold rounded-lg text-sm px-5 py-2.5">
           Log out
         </button>
         <button v-else
           type="button"
-          class="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300
-                font-medium rounded-lg text-sm px-5 py-2.5">
+          class="text-white bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300
+                font-bold rounded-lg text-sm px-5 py-2.5">
           Log in
         </button>
       </div>
@@ -29,6 +34,10 @@ export default {
   name: 'NavigationBar',
   props: {
     isLoggedIn: {
+      type: Boolean,
+      default: false
+    },
+    isDashboard: {
       type: Boolean,
       default: false
     }
