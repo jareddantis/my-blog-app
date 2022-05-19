50ba21a15d288596386ba13e3705e85206ba5e28
Jared Dantis <jareddantis@gmail.com>
2022-05-19 14:35:39 +0800

    nuxt: Import Google Fonts CSS in head
    The Google Fonts module for Nuxt does not properly handle optical sizing for variable fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,10 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', preconnect: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,400;8..144,700&family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
     ]
   },
 
@@ -31,9 +34,7 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    // https://google-fonts.nuxtjs.org
-    '@nuxtjs/google-fonts'
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -59,21 +60,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-
-  // Google Fonts
-  googleFonts: {
-    display: 'swap',
-    families: {
-      'Roboto+Flex': {
-        wght: [400, 700],
-        ital: [400, 700]
-      },
-      Lora: {
-        wght: [400, 700],
-        ital: [400, 700]
-      }
-    }
   },
 
   target: 'static'
