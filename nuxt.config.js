c8bc5bb60a53def25205d4b19c9b3c89fee52b32
Jared Dantis <jareddantis@gmail.com>
2022-05-19 15:19:40 +0800

    nuxt: Also import Roboto Flex 700 with slight slant

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', preconnect: true },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,400;8..144,700&family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,slnt,wght@8..144,-5,700;8..144,0,400;8..144,0,700&family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
     ]
   },
 
