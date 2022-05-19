99c33bfa948a796a059f4b8b845310db589e0944
Jared Dantis <jareddantis@gmail.com>
2022-05-19 18:21:33 +0800

    NavigationBar: Wrap buttons in nuxt-links

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -12,18 +12,20 @@
                 focus:ring-4 focus:ring-teal-300 font-bold rounded-lg text-sm px-5 py-2.5 mr-3">
           Dashboard
         </button>
-        <button v-if="isLoggedIn"
-          type="button"
-          class="text-teal-600 bg-white border-2 border-teal-600 hover:border-teal-800 focus:ring-4
-                focus:ring-teal-300 hover:text-teal-800 font-bold rounded-lg text-sm px-5 py-2.5">
-          Log out
-        </button>
-        <button v-else
-          type="button"
-          class="text-white bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300
-                font-bold rounded-lg text-sm px-5 py-2.5">
-          Log in
-        </button>
+        <nuxt-link v-if="isLoggedIn">
+          <button type="button"
+            class="text-teal-600 bg-white border-2 border-teal-600 hover:border-teal-800 focus:ring-4
+                  focus:ring-teal-300 hover:text-teal-800 font-bold rounded-lg text-sm px-5 py-2.5">
+            Log out
+          </button>
+        </nuxt-link>
+        <nuxt-link v-else to="/login">
+          <button type="button"
+            class="text-white bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300
+                  font-bold rounded-lg text-sm px-5 py-2.5">
+            Log in
+          </button>
+        </nuxt-link>
       </div>
     </div>
   </nav>
