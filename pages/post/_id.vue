a356b4f460b2e375e434ec3a1490486e056f5439
Jared Dantis <jareddantis@gmail.com>
2022-05-20 03:53:34 +0800

    post: Properly show post from Firestore

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,40 +1,62 @@
 <template>
-  <div class="bg-white bg-full">
-    <!-- Navbar -->
-    <NavigationBar is-logged-in />
+  <DefaultPage>
+    <!-- Post title -->
+    <h1 class="md:text-6xl text-4xl font-bold font-slight-italic mb-4">
+      {{ title }}
+    </h1>
 
-    <!-- Main content -->
-    <div class="md:max-w-2xl max-w-xl w-full mx-auto py-3 px-6">
-      <!-- Post title -->
-      <h1 class="md:text-6xl text-4xl font-bold font-slight-italic mb-4">
-        My blog
-      </h1>
+    <!-- Post author and date -->
+    <h2 class="text-xl text-teal-500">
+      {{ authorName }}<br>
+      {{ timestamp }}
+    </h2>
 
-      <!-- Post author and date -->
-      <h2 class="text-xl text-teal-500">
-        by Author<br>May 21, 2022
-      </h2>
-
-      <!-- Post content -->
-      <article class="prose mt-8 mx-auto max-w-full w-full">
-        <p class="font-serif">
-          Morbi placerat congue nisi et eleifend. Nam cursus cursus orci eget molestie.
-          Donec libero lorem, porta quis gravida id, tempor ut arcu. Mauris in posuere odio.
-          In in eleifend augue. Phasellus vitae egestas tellus. Mauris non posuere elit,
-          ut tincidunt felis. Donec sit amet leo vel ante congue vulputate et ut elit.
-          Nam ipsum nisi, ultrices vitae ullamcorper non, luctus id lectus. Vivamus vulputate
-          nec massa at laoreet. Etiam sit amet blandit purus.
-        </p>
-      </article>
-    </div>
-  </div>
+    <!-- Post content -->
+    <article class="prose mt-8 mx-auto max-w-full w-full">
+      <p class="font-serif">
+        {{ content }}
+      </p>
+    </article>
+  </DefaultPage>
 </template>
 
 <script>
-import NavigationBar from '~/components/NavigationBar.vue'
+import DefaultPage from '~/components/DefaultPage.vue'
+
 export default {
   name: 'PostPage',
-  components: { NavigationBar }
+  components: { DefaultPage },
+  data () {
+    return {
+      title: 'Loading post...',
+      authorName: '',
+      content: '',
+      timestamp: ''
+    }
+  },
+  mounted () {
+    this.fetchPost()
+  },
+  methods: {
+    fetchPost () {
+      // Get post ID from the route
+      const postId = this.$route.params.id
+
+      this.$fire.firestore
+        .collection('blog')
+        .doc(postId)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            const post = doc.data()
+            this.title = post.title
+            this.authorName = 'by ' + post.author
+            this.content = post.content
+            this.timestamp = post.date.toDate().toDateString()
+          }
+        })
+    }
+  }
 }
 </script>
 
