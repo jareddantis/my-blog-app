c9c6619330e1020005206025cc0f57814813d3e6
Jared Dantis <jareddantis@gmail.com>
2022-05-19 19:57:37 +0800

    nuxt: Import Firebase Auth service worker

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,14 @@ export default {
   pwa: {
     manifest: {
       lang: 'en'
+    },
+    workbox: {
+      importScripts: [
+        '/firebase-auth-sw.js'
+      ],
+      // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
+      // only set this true for testing and remember to always clear your browser cache in development
+      dev: process.env.NODE_ENV === 'development'
     }
   },
 
@@ -76,14 +84,11 @@ export default {
       measurementId: process.env.FIREBASE_MEASUREMENT_ID
     },
     services: {
-      auth: true,
+      auth: {
+        ssr: true
+      },
       firestore: true
     }
-  },
-
-  // Firebase auth config
-  auth: {
-    ssr: true
   },
 
   target: 'static'
