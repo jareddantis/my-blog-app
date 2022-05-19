69b612551bb9d4ea0f03c4cdfbc7a5cc1795ce8e
Jared Dantis <jareddantis@gmail.com>
2022-05-20 03:36:36 +0800

    components: Create custom `<button>` with loading effect

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -2,7 +2,7 @@
   <!-- https://flowbite.com/docs/components/spinner/ -->
   <svg
     role="status"
-    class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+    class="w-8 h-8 mr-2 animate-spin dark:text-teal-600 fill-gray-600 dark:fill-gray-300"
     viewBox="0 0 100 101"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -20,6 +20,16 @@
 
 <script>
 export default {
-  name: 'CircularSpinner'
+  name: 'SimpleSpinner',
+  props: {
+    bgColor: {
+      type: String,
+      default: 'blue'
+    },
+    fgColor: {
+      type: String,
+      default: 'white'
+    }
+  }
 }
 </script>
---
+++ components/SpinnerButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button type="button" :class="actualStyle" @click="$emit('click')">
+    <SimpleSpinner v-if="spinning" />
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+import SimpleSpinner from '~/components/Spinner.vue'
+
+export default {
+  name: 'SpinnerButton',
+  components: { SimpleSpinner },
+  props: {
+    buttonStyle: {
+      type: String,
+      default:
+        'bg-teal-500 rounded-md p-3 w-full text-white font-bold hover:bg-teal-700 cursor-pointer'
+    },
+    spinning: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    actualStyle () {
+      const definedStyle = this.buttonStyle
+      const spinningStyle = this.spinning
+        ? 'flex justify-center items-center'
+        : ''
+      return `${definedStyle} ${spinningStyle}`
+    }
+  }
+}
+</script>
